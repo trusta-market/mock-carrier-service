3a72b4fb31d7f1b90252d1a6a7ec126e53cb80e8
Seungwon-Choi <bestwondev@gmail.com>
2026-05-07 15:31:28 +0900

    chore: Spring Boot 프로젝트 초기화

---
+++ src/main/java/com/trustamarket/mockcarrierservice/MockCarrierServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trustamarket.mockcarrierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockCarrierServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockCarrierServiceApplication.class, args);
+    }
+
+}
